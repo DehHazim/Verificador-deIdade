948f897b711b683dbea16d0fde9697d401107937
Deborah Hazim <deborahhhazim@gmail.com>
2025-05-05 15:48:56 -0300

    JS
    Atualizando o programa em JS

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function verifcar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
-    if (fano.ariaValueMax.length == 0 || Number(fano.value) < value) {
-        
-    }
+function verificar() {
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fano = document.getElementById('txtano')
+   var res = document.querySelector('div#res') // poderia ser o usado o getElementById
+   if (fano.value.length == 0 || fano.value > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente')
+   } else {
+    window.alert('Tudo ok!')
+   }
+
 }
